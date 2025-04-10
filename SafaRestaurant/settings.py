536060d4fe12609495faf78ecbe81c6e41c18d91
Sebastán <aramayosebas10@gmail.com>
2025-04-10 20:18:07 +0200

    oracle

--- SafaRestaurant/settings.py
+++ SafaRestaurant/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'SafaRestaurant.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.oracle',
+        'NAME': 'xe',
+        'USER': 'C##SEBAS',
+        'PASSWORD': '1234',
+        'HOST': 'localhost',
+        'PORT': '1521',
     }
 }
 
