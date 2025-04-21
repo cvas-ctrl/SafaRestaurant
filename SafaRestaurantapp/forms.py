9b64c339496299b482e6d49911d7697e32911e93
Sebastán <aramayosebas10@gmail.com>
2025-04-21 17:08:12 +0200

    RAMA PARA CONTENIDOS GENERALES

--- SafaRestaurantapp/forms.py
+++
@@ -1,7 +0,0 @@
-from django import forms
-from .models import Camarero
-
-class CamareroForm(forms.ModelForm):
-    class Meta:
-        model = Camarero
-        fields = ['nombre', 'apellidos', 'dni', 'email', 'fecha_nacimiento']
